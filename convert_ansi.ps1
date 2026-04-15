f83cf205f35e975c0c4962201e571c6b90c2f210
Dreamtrust <dreamtrust@users.noreply.github.com>
2026-04-16 01:23:34 +0500

    fix: update old path in convert_ansi.ps1

--- convert_ansi.ps1
+++ convert_ansi.ps1
@@ -1,4 +1,4 @@
-$path = 'C:\Games\Gothic_Dev\SimpleHello\src\Plugin.hpp'
+﻿$path = 'C:\Games\Gothic_Dev\zSimpleAcceleration\src\Plugin.hpp'
 # Читаем исходный файл как UTF8 (без BOM), чтобы не потерять кириллицу при чтении
 $content = Get-Content -Path $path -Encoding UTF8
 # Записываем обратно в 1251 (ANSI)
